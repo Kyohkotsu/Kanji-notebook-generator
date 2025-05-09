2f4989c8613aa919cdf1e56021a18592723b553e
Thierry Dorion <thierrydorion@hotmail.com>
2025-05-09 20:04:05 +0000

    Updated kanjiScraping.py

--- kanjiScraping.py
+++ kanjiScraping.py
@@ -1,67 +1,83 @@
-# from docx import Document
+import
 import requests
-import tkinter as tk
 from bs4 import BeautifulSoup
+import tkinter as tk
+from tkinter import scrolledtext
 
-def jisho_search(kanji):
-    url = f"https://jisho.org/search/{kanji}%20%23kanji"
-    headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print("Erreur de connexion")
-        return [], []
+class KanjiScrapingApp(tk.Frame):
+    """Ce programme fait du scraping web et génère un fichier pdf de pratique de kanji"""
+    def __init__(self, master: tk):
+        super().__init__(master)
+        master.title("漢字スクレイピング")
+        self.pack()
+        self.create_widgets()
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    def create_widgets(self):
+        self.kanjiInput_label = tk.Label(self, text="Saisir les kanjis: ")
+        self.kanjiInput_label.pack(pady=(5, 0), padx=5, anchor=tk.W)
 
-    readings_section = soup.select_one(".kanji-details__main-readings")
-    if not readings_section:
-        print("***Erreur : Ce kanji n'existe pas***")
+        self.kanjiInput_entry = tk.Entry(self, width=50)
+        self.kanjiInput_entry.pack(pady=5, padx=5)
 
-    kunyomi = []
-    kun_section = soup.select_one(".kanji-details__main-readings dl.kun_yomi")
-    if kun_section:
-        kunyomi = [a.get_text() for a in kun_section.select("a")]
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack()
 
-    onyomi = []
-    on_section = soup.select_one(".kanji-details__main-readings dl.on_yomi")
-    if on_section:
-        onyomi = [a.get_text() for a in on_section.find_all("a")]
+        self.get_reply_button = tk.Button(self.button_frame, bg="skyblue", text="Rechercher", command=self.get_request, cursor="hand2")
+        self.get_reply_button.pack(side=tk.LEFT, pady=5)
 
-    return kunyomi, onyomi
+        self.response_label = tk.Label(self, text="Log: ")
+        self.response_label.pack(pady=(5, 0), padx=5, anchor=tk.W)
 
-def read_kanji():
-    kanji = kanjiEntry.get()
-    kanji = list(kanji)
-    n = 1
-    for c in kanji:
-        kunyomi, onyomi = jisho_search(c)
-        answerbox.insert(tk.END, f"漢字{n}: {c}\n")
-        answerbox.insert(tk.END, f"訓読み (Kunyomi): {kunyomi}\n")
-        answerbox.insert(tk.END, f"音読み (Onyomi): {onyomi}\n")
-        n += 1
+        self.response_text = scrolledtext.ScrolledText(self, width=50)
+        self.response_text.pack(pady=5, padx=5)
 
-# Créer l'instance fenetre
-fenetre = tk.Tk()
+    def get_request(self):
+        kanjiList = self.kanjiInput_entry.get().strip()
+        self.response_text.insert(tk.END, f"Recherche {kanjiList}\n")
+        if not kanjiList:
+            self.response_text.insert(tk.END, "Erreur, veuillez saisir des kanjis.\n")
+            return
+        else:
+            kanjiList = list(kanjiList)
+            i = 1
+            for c in kanjiList:
+                kunyomi, onyomi = self.get_readings(c)
+                self.response_text.insert(tk.END, f"{i}. {c}")
+                self.response_text.insert(tk.END, f"\n訓読み (Kunyomi): {str(kunyomi)}")
+                self.response_text.insert(tk.END, f"\n音読み (Onyomi) : {str(onyomi)} \n")
+                i += 1
 
-# Donner un titre à la fenêtre
-fenetre.title("Trouver la prononciation de tes kanjis")
+    def get_readings(self, kanji):
+        url = f"https://jisho.org/search/{kanji}%20%23kanji"
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers=headers)
 
-# Configurer dex labels, deux textbox, un bouton de confirmation
-entryLabel = tk.Label(fenetre, text="Saisir tes kanjis:")
-kanjiEntry = tk.Entry(fenetre)
-buttonFrame = tk.Frame(fenetre)
-confirmButton = tk.Button(buttonFrame, text="Rechercher", command=read_kanji)
-answerLabel = tk.Label(fenetre, text="Saisir tes kanjis:")
-answerbox = tk.Entry(fenetre)
+        if response.status_code != 200:
+            self.response_text.insert(tk.END, "Erreur de connexion. Vérifiez la connexion internet.\n")
+            return [], []
 
-# Placer les éléments dans fenetre
-entryLabel.pack(anchor=tk.W)
-kanjiEntry.pack()
-buttonFrame.pack()
-confirmButton.pack(pady=5, padx=5)
-answerLabel.pack(anchor=tk.W)
-answerbox.pack()
+        soup = BeautifulSoup(response.text, "html.parser")
 
-# Lancer fenetre
-fenetre.mainloop()
+        readings_section = soup.select_one(".kanji-details__main-readings")
+        if not readings_section:
+            self.response_text.insert(tk.END, "***Erreur : Ce kanji n'existe pas***\n")
+
+        kunyomi = []
+        kun_section = soup.select_one(".kanji-details__main-readings dl.kun_yomi")
+        if kun_section:
+            kunyomi = [a.get_text() for a in kun_section.select("a")]
+
+        onyomi = []
+        on_section = soup.select_one(".kanji-details__main-readings dl.on_yomi")
+        if on_section:
+            onyomi = [a.get_text() for a in on_section.find_all("a")]
+
+        return kunyomi, onyomi
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    fenetre = KanjiScrapingApp(root)
+    fenetre.mainloop()
+
